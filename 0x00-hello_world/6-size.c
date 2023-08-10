5df974a8438d64a6413144b4c71725d429f1f88f
umohp <umoh4000@gmail.com>
2023-08-10 11:33:46 -0700

    0x00 Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #!/bin/bash
 #include<stdio.h>
 /**
- *main-Entry point 
- *
- *Return: Always 0 (success);
- */
- *int main (void)
+*main-Entry point 
+*
+*Return: Always 0 (success);
+*/
+*int main (void)
 {
 	printf("size of a char: %d byte(s)\n", size of (char));
 	printf("size of an int: %d byte(s)\n", size of (int));
