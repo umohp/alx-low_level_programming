6eb5f1dd6c11b1fb0a5ea00f0b72e69de2418280
umohp <umoh4000@gmail.com>
2023-08-10 15:22:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
  *main-Entry point 
  *
- *Return: Always 0(success)
+ *return:0(success)
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("programming is like building a multilingual puzzle\n");
 	return(0);
 }
