4b5c30dcb1f5739bc91d8dfe401864093f930955
umohp <umoh4000@gmail.com>
2023-08-10 16:10:29 -0700

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
-#!/bin/bash
 #include<stdio.h>
 /**
-*main-Entrypoint
-*
-*Return: Always 0 (success)
-*/
+ *main-Entrypoint
+ *
+ *return: Always 0 (success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
