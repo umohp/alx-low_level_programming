71999956c8644da377157ba1beaf855f582ebd02
umohp <umoh4000@gmail.com>
2023-08-11 03:50:53 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
-#include<unistd.h>
+#include <unistd.h>
 
 /**
- * main - print "and that piece of art is useful" - Dora Korpar, 2015-10-19".
- * followed by a new line, to standard error
- *Return: Always 1 (success);
+ * main - print "and that piece of art is useful" - Dora korpar, 2015-10-19",
+ * follwed by a new line, to standard error .
+ * Return: Always 1.
  */
-int main (void)
+
+
+int main(void)
+
 {
-	write (2, "and that piece of art is useful\n - Dora Korpar, 2015-10-19\n", 59)
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
