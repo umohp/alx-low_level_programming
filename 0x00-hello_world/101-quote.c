1b4944e35671477f3288dac6cd2b8622ff4cf1c7
umohp <umoh4000@gmail.com>
2023-08-10 18:01:37 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include<stdio.h>
 #include<unistd.h>
 
@@ -7,8 +6,8 @@
  *
  *Return: Always 1 (success);
  */
- int main (void)
- {
+int main (void)
+{
 	write (2  
 	"and that piece of art is useful\n - Dora Korpar, 2015-10-19\n", 59)
 	return (1);
