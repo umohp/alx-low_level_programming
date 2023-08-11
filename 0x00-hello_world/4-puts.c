407efa140783a361825b417bd11542ff59e14970
umohp <umoh4000@gmail.com>
2023-08-11 03:04:17 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
+
 /**
- *main-Entry point 
- *
+ *main - prints "Programming is like building a multilingual
+ *puzzle, follwed by a new line."
  *return:0(success)
  */
 int main(void)
