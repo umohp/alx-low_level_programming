67e3cb1ed65174cbaf74641e5a15d30a3b947107
umohp <umoh4000@gmail.com>
2023-08-11 03:11:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,11 @@
 /**
  *main - prints "Programming is like building a multilingual
  *puzzle, follwed by a new line."
- *return:0(success)
+ *Return: Always 0(success).
  */
 int main(void)
 {
 	puts("\" programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
+
 }
